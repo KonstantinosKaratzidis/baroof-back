bf519bea850e62b4925abe50c77e7bfa3e5f6abe
Konstantinos Karatzidis <kostaskarat@hotmail.com>
2022-04-08 02:21:12 +0300

    Implemented signup in the backend

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const signupRouter = require("./signup.js");
+const cors = require("cors");
 
 const router = express.Router();
 module.exports = router;
+
+router.use(bodyParser.json());
+
+// TODO: change for development and production
+router.use(cors({
+	origin: "http://localhost:3000"
+}))
+
 
 router.get("/login", (req, res) => {
 	res.status(200).end("Check if logged in");
@@ -11,6 +22,4 @@ router.post("/login", (req, res) => {
 	res.status(200).end("Login");
 })
 
-router.post("/signup", (req, res) => {
-	res.status(200).end("signup");
-})
+router.use("/signup", signupRouter);
---
+++ src/routes/api/signup.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const User = require("../../models/User.js");
+const bcrypt = require("bcrypt");
+const {body, validationResult} = require("express-validator");
+
+router = express.Router();
+module.exports = router;
+
+// validation middleware
+router.post("/", 
+	body("email", "Invalid email").isEmail().normalizeEmail(),
+	body("nickname", "Nickname cannot be empty").trim().not().isEmpty(),
+	body("password", "At least 6 characters").isStrongPassword({
+		minLength: 6, minLowercase: 0, minUppercase: 0,
+		minNumbers: 0, minSymbols: 0
+	}),
+	(req, res, next) => {
+		const result = validationResult(req);
+		try {
+			result.throw();
+			console.log("validation OK");
+			next();
+		} catch(err) {
+			const errors = result.array().map(({param, msg}) => ({param, msg}))
+			res.json({
+				success: false,
+				msg: "Validation failed",
+				errors
+			})
+		}
+	}
+)
+
+router.post("/", async (req, res) => {
+	const {email, nickname, password} = req.body;
+	const user = new User({email, nickname, password})
+	try {
+		await user.save();
+	} catch(err) {
+		if(err.code === 11000){ // duplicate (email taken)
+			return res.json({
+				success: false,
+				msg: "Email taken",
+				errors: [{param: "email", msg: "E-mail not available"}]
+			})
+		} else {
+			console.log("unknown error", err)
+			return res.json({
+				sucess: false,
+				msg: "Unknown error"
+			})
+		}
+	}
+	res.status(200).json({
+		success: true,
+		msg: "User created"
+	});
+})
